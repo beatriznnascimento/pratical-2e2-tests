b909a9922ddb65092a571a044df3c58205c8ed52
beatriznnascimento <beatriznascimento@alu.ufc.br>
2025-02-02 20:51:25 -0300

    test: ensure user can view product details and add to cart

--- Pratical-e2e-test-main/cypress/e2e/Inventory Test/2_products.cy.js
+++ Pratical-e2e-test-main/cypress/e2e/Inventory Test/2_products.cy.js
@@ -8,7 +8,26 @@ describe('Inventory - Products', () => {
     doSignIn(users.standard_user);
   });
 
-  it.skip('Should see the product details and add to cart');
+  it('Should see the product details and add to cart', () => {
+    cy.visit(url);
+
+    cy.get('[data-test="username"]').type(users.standard_user.username);
+    cy.get('[data-test="password"]').type(users.standard_user.password);
+    cy.get('[data-test="login-button"]').click();
+
+    cy.url().should('include', '/inventory.html');
+    cy.get('.title').should('have.text', 'Products');
+
+    cy.get('.inventory_item').first().find('.inventory_item_name').click();
+
+    cy.get('.inventory_details_name').should('be.visible');
+
+    cy.get('[data-test^="add-to-cart"]', { timeout: 10000 })
+        .should('be.visible')
+        .click();
+
+    cy.get('.shopping_cart_badge').should('contain.text', '1');
+});
 
   it.skip('Should sort products by price properly (high to low)');
 
