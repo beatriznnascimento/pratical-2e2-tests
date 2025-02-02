3146f8ff3d2652d25be205d4e73c7539f7e812d3
beatriznnascimento <beatriznascimento@alu.ufc.br>
2025-02-02 20:59:51 -0300

    test: ensure checkout process follows correct flow

--- Pratical-e2e-test-main/cypress/e2e/Inventory Test/3_checkout.cy.js
+++ Pratical-e2e-test-main/cypress/e2e/Inventory Test/3_checkout.cy.js
@@ -8,7 +8,34 @@ describe("Inventory - Products", () => {
     doSignIn(users.standard_user);    
   });
 
-  it.skip("Should do checkout with the correct flow");
+  it("Should do checkout with the correct flow", () => {
+    cy.visit(url);
+  
+    cy.get('[data-test="username"]').type(users.standard_user.username);
+    cy.get('[data-test="password"]').type(users.standard_user.password);
+    cy.get('[data-test="login-button"]').click();
+  
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  
+    cy.get('.shopping_cart_link').click();
+  
+    cy.get('.cart_item').should('have.length', 2);
+  
+    cy.get('[data-test="checkout"]').click();
+
+    cy.get('[data-test="firstName"]').type('John');
+    cy.get('[data-test="lastName"]').type('Doe');
+    cy.get('[data-test="postalCode"]').type('12345');
+
+    cy.get('[data-test="continue"]').click();
+  
+    cy.get('.cart_item').should('have.length', 2);
+
+    cy.get('[data-test="finish"]').click();
+
+    cy.get('.complete-header').should('have.text', 'Thank you for your order!');
+  });
 
   it.skip("Should select some products, go to cart, and go back to continue shopping");
 
