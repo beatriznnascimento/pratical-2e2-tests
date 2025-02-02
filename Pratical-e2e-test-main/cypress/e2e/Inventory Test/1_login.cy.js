92ee371d44eb423ab24c3759a56118a57d41d3cd
beatriznnascimento <beatriznascimento@alu.ufc.br>
2025-02-02 20:39:20 -0300

    test: verify login and logout functionality

--- Pratical-e2e-test-main/cypress/e2e/Inventory Test/1_login.cy.js
+++ Pratical-e2e-test-main/cypress/e2e/Inventory Test/1_login.cy.js
@@ -11,8 +11,23 @@ describe('Inventory - Login', () => {
       cy.contains('Products');
     });
     
-    it.skip('Should login with valid credentials and do logout');
+    it('Should login with valid credentials and do logout', () => {
+      cy.visit(url); 
 
+      cy.get('[data-test="username"]').type(users.standard_user.username);
+      cy.get('[data-test="password"]').type(users.standard_user.password);
+      cy.get('[data-test="login-button"]').click();
+    
+      cy.url().should('include', '/inventory.html');
+      cy.get('.title').should('have.text', 'Products');
+    
+      cy.get('#react-burger-menu-btn').click();
+      cy.get('#logout_sidebar_link').click();
+    
+      cy.url().should('include', '/');
+      cy.get('[data-test="login-button"]').should('be.visible');
+    });
+    
     it.skip('Should not login with invalid credentials');
 
     it.skip('Should not allow "locked_out_user" do sign in');
