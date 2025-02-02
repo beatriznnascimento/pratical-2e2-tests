c40f939a12dc1c289454cdcb7b780d409552b1f5
beatriznnascimento <beatriznascimento@alu.ufc.br>
2025-02-02 20:53:30 -0300

    test: ensure products are sorted by price (high to low)

--- Pratical-e2e-test-main/cypress/e2e/Inventory Test/2_products.cy.js
+++ Pratical-e2e-test-main/cypress/e2e/Inventory Test/2_products.cy.js
@@ -29,7 +29,28 @@ describe('Inventory - Products', () => {
     cy.get('.shopping_cart_badge').should('contain.text', '1');
 });
 
-  it.skip('Should sort products by price properly (high to low)');
+  it('Should sort products by price properly (high to low)', () => {
+    cy.visit(url);
+
+    cy.get('[data-test="username"]').type(users.standard_user.username);
+    cy.get('[data-test="password"]').type(users.standard_user.password);
+    cy.get('[data-test="login-button"]').click();
+
+
+    cy.url().should('include', '/inventory.html');
+    cy.get('.title').should('have.text', 'Products');
+
+    cy.get('.product_sort_container').select('hilo');
+
+    cy.get('.inventory_item_price').then(($prices) => {
+      const prices = [...$prices].map((price) =>
+        parseFloat(price.innerText.replace('$', ''))
+      );
+
+      const sortedPrices = [...prices].sort((a, b) => b - a);
+      expect(prices).to.deep.equal(sortedPrices);
+    });
+  });
 
   it.skip('Should sort products by price properly (low to high)');
 });
