49664bfdd2828c807f81c65c2c1cd60dd90f8bad
beatriznnascimento <beatriznascimento@alu.ufc.br>
2025-02-02 21:01:41 -0300

    test: ensure user can return to inventory after viewing cart

--- Pratical-e2e-test-main/cypress/e2e/Inventory Test/3_checkout.cy.js
+++ Pratical-e2e-test-main/cypress/e2e/Inventory Test/3_checkout.cy.js
@@ -37,7 +37,25 @@ describe("Inventory - Products", () => {
     cy.get('.complete-header').should('have.text', 'Thank you for your order!');
   });
 
-  it.skip("Should select some products, go to cart, and go back to continue shopping");
-
+  it("Should select some products, go to cart, and go back to continue shopping", () => {
+    cy.visit(url);
+  
+    cy.get('[data-test="username"]').type(users.standard_user.username);
+    cy.get('[data-test="password"]').type(users.standard_user.password);
+    cy.get('[data-test="login-button"]').click();
+  
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  
+    cy.get('.shopping_cart_link').click();
+  
+    cy.get('.cart_item').should('have.length', 2);
+  
+    cy.get('[data-test="continue-shopping"]').click();
+  
+    cy.url().should('include', '/inventory.html');
+    cy.get('.title').should('have.text', 'Products');
+  });
+  
   it.skip("Should not continue checkout with empty delivery information");
 });
